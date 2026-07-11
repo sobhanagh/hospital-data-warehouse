e84ac66a80da32f56a55edd68c18f6b4db1d4728
mhrn83 <mehranstock1383@gmail.com>
2026-07-11 20:42:44 +0330

    feat(DW): Add ETL control table
    Add table to track the status of previous load process on tables.

---
+++ DW/Facts/Common.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ETL_Control (
+    Table_Name VARCHAR(100) NOT NULL PRIMARY KEY,
+    Last_Processed_ID INT NOT NULL DEFAULT 0,
+    Last_Run_Status VARCHAR(20) NOT NULL, -- 'SUCCESS', 'FAILED'
+    Last_Run_Timestamp DATETIME DEFAULT GETDATE(),
+    Error_Message TEXT
+);

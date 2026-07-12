e09c2e69a2b25145cc257e4a59e87bad99692d5c
sobhanagh <sobhanaghajani893@gmail.com>
2026-07-12 17:23:13 +0330

    feat(common): add Last_Processed_Date to ETL_Control

--- DW/Facts/Common.sql
+++ DW/Facts/Common.sql
@@ -3,5 +3,6 @@ CREATE TABLE ETL_Control (
     Last_Processed_ID INT NOT NULL DEFAULT 0,
     Last_Run_Status VARCHAR(20) NOT NULL, -- 'SUCCESS', 'FAILED'
     Last_Run_Timestamp DATETIME DEFAULT GETDATE(),
+    Last_Processed_Date DATE,
     Error_Message TEXT
 );
